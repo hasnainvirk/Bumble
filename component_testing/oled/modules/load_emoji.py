c27c35c9eb8dda2e877d9c7618a07d1b0682c950
Hasnain Virk <husnain.virk@gmail.com>
2024-08-24 04:17:24 +0300

    Adding Angry and Sad emojis

--- component_testing/oled/modules/load_emoji.py
+++ component_testing/oled/modules/load_emoji.py
@@ -19,12 +19,7 @@ class Emoji(Oled):
             self.log.error(f"Invalid emoji number: {emoji}")
             return
 
-        if emoji == HAPPY:
-            self.__load_happy()
-        elif emoji == SAD:
-            self.log.error("SAD emoji not implemented yet.")
-        elif emoji == ANGRY:
-            self.log.error("ANGRY emoji not implemented yet.")
+        self.__render(emoji)
 
     # Function to draw a smiley face
     def __draw_happy_emoji(self, draw, pos, radius, eyes_open=True):
@@ -78,13 +73,94 @@ class Emoji(Oled):
             fill=255,
         )
 
-    def __load_happy(self):
+    def __draw_angry_emoji(self, draw, pos, eyes_open=True):
+        eye_radius = 3
+        eye_y = HEIGHT_PX // 2 - 10
+        if eyes_open:
+            draw.polygon(
+                [
+                    (pos - 10 - eye_radius, eye_y - eye_radius),
+                    (pos - 10, eye_y),
+                    (pos - 10 + eye_radius, eye_y - eye_radius),
+                ],
+                outline=255,
+                fill=255,
+            )
+            draw.polygon(
+                [
+                    (pos + 10 - eye_radius, eye_y - eye_radius),
+                    (pos + 10, eye_y),
+                    (pos + 10 + eye_radius, eye_y - eye_radius),
+                ],
+                outline=255,
+                fill=255,
+            )
+        else:
+            draw.line(
+                (pos - 10 - eye_radius, eye_y, pos - 10 + eye_radius, eye_y), fill=255
+            )
+            draw.line(
+                (pos + 10 - eye_radius, eye_y, pos + 10 + eye_radius, eye_y), fill=255
+            )
+        # Draw angry mouth
+        draw.line(
+            [(pos - 15, HEIGHT_PX // 2 + 20), (pos + 15, HEIGHT_PX // 2 + 20)],
+            fill=255,
+        )
+
+    def __draw_sad_emoji(self, draw, pos, eyes_open=True):
+        eye_radius = 3
+        eye_y = HEIGHT_PX // 2 - 10
+        if eyes_open:
+            draw.ellipse(
+                (
+                    pos - 10 - eye_radius,
+                    eye_y - eye_radius,
+                    pos - 10 + eye_radius,
+                    eye_y + eye_radius,
+                ),
+                outline=255,
+                fill=255,
+            )
+            draw.ellipse(
+                (
+                    pos + 10 - eye_radius,
+                    eye_y - eye_radius,
+                    pos + 10 + eye_radius,
+                    eye_y + eye_radius,
+                ),
+                outline=255,
+                fill=255,
+            )
+        else:
+            draw.line(
+                (pos - 10 - eye_radius, eye_y, pos - 10 + eye_radius, eye_y), fill=255
+            )
+            draw.line(
+                (pos + 10 - eye_radius, eye_y, pos + 10 + eye_radius, eye_y), fill=255
+            )
+        # Draw frown
+        draw.arc(
+            [(pos - 15, HEIGHT_PX // 2 + 10), (pos + 15, HEIGHT_PX // 2 + 30)],
+            start=180,
+            end=360,
+            fill=255,
+        )
+
+    def __draw(self, etype, draw, pos, radius, eyes_open=True):
+        if etype == HAPPY:
+            self.__draw_happy_emoji(draw, pos, radius, eyes_open)
+        elif etype == SAD:
+            self.__draw_sad_emoji(draw, pos, eyes_open)
+        elif etype == ANGRY:
+            self.__draw_angry_emoji(draw, pos, eyes_open)
+
+    def __render(self, etype: str):
         # Create a blank image for drawing.
         image = Image.new("1", (WIDTH_PX, HEIGHT_PX))
         # Create a drawing object.
         draw = ImageDraw.Draw(image)
         # Initialize variables
-
         pos = WIDTH_PX // 2
         eyes_open = True
 
@@ -92,8 +168,8 @@ class Emoji(Oled):
             # Clear image buffer by drawing a black filled box.
             draw.rectangle((0, 0, WIDTH_PX, HEIGHT_PX), outline=0, fill=0)
 
-            # Draw the smiley face with eyes open or closed
-            self.__draw_happy_emoji(draw, pos, HEIGHT_PX // 2 - 15, eyes_open)
+            # Draw the emoji face with eyes open or closed
+            self.__draw(etype, draw, pos, HEIGHT_PX // 2 - 15, eyes_open)
 
             # Update the display with the new image
             self.disp.image(image)
