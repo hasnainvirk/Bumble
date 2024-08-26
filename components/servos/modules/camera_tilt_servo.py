f7f4838907c286a3abf8da58d95d2fdd2e09cb1c
Hasnain Virk <hasnain@tinysio.com>
2024-08-26 04:51:10 +0300

    Fixing typo

--- components/servos/modules/camera_tilt_servo.py
+++ components/servos/modules/camera_tilt_servo.py
@@ -3,6 +3,7 @@ import RPi.GPIO as GPIO
 import time, logging
 
 GPIO_6 = 6
+POINTING_CENTER_ANGLE = 90
 POINTING_DOWN_ANGLE = 180
 POINTING_UP_ANGLE = 0
 
@@ -14,6 +15,11 @@ class CameraTiltServo(ServoIface):
         self.current_angle = POINTING_DOWN_ANGLE
         self.tilt_down()
 
+    def tilt_center(self):
+        self.log.debug("Camera Servo - Tilting Center")
+        self.generate_pulse(POINTING_DOWN_ANGLE)
+        self.__set_angle(POINTING_DOWN_ANGLE)
+
     def tilt_down(self):
         self.log.debug("Camera Servo - Tilting Down")
         for _ in range(0, 50):
@@ -22,7 +28,7 @@ class CameraTiltServo(ServoIface):
     def tilt_up(self):
         self.log.debug("Camera Servo - Tilting Up")
         for _ in range(0, 50):
-            self.generate_pulse(POINTING_UP_ANGLE=0)
+            self.generate_pulse(POINTING_UP_ANGLE)
 
     def __set_angle(self, angle):
         self.current_angle = angle
